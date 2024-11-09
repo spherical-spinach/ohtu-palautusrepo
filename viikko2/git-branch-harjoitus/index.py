221039c56dfea952170fc076fc36524ea02f6ded
asdf <eemeli.paalimaki.work@protonmail.com>
2024-11-09 21:31:30 +0200

    Modify index.py

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -4,13 +4,13 @@ from logger import logger
 from summa import summa
 from erotus import erotus
 
-logger("aloitetaan")
+logger("aloitetaan ohjelma")
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
-print(f"{summa(x, y)}")
-print(f"{erotus(x, y)}")
+print(f"Lukujen {x} ja {y} summa on {summa(x, y)}")  # muutos bugikorjaus-branchissa
+print(f"Lukujen {x} ja {y} erotus on {erotus(x, y)}")  # muutos bugikorjaus-branchissa
 
-logger("lopetetaan")
+logger("lopetetaan ohjelma")
+print("goodbye!")
 
-# bugikorjaus
