0a67b7090c8fefe32af3535aa8bec1b8787e8cac
asdf <eemeli.paalimaki.work@protonmail.com>
2024-11-11 16:05:37 +0200

    Complete some merge I don't have a clue about

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -4,14 +4,12 @@ from logger import logger
 from summa import summa
 from erotus import erotus
 
-logger("aloitetaan")
+logger("aloitetaan ohjelma")
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
-print(f"{summa(x, y)}") # muutos mainissa
-print(f"{erotus(x, y)}") # muutos mainissa
+print(f"Lukujen {x} ja {y} summa on {summa(x, y)}") # muutos mainissa  # muutos bugikorjaus-branchissa
+print(f"Lukujen {x} ja {y} erotus on {erotus(x, y)}") # muutos mainissa  # muutos bugikorjaus-branchissa
 
 logger("lopetetaan")
-print("goodbye!")
 
-# bugikorjaus
