615c0efdf20456dbfa1362366bc944e7ba75d266
asdf <eemeli.paalimaki.work@protonmail.com>
2024-11-17 11:57:05 +0200

    Complete week3 exercise 5

---
+++ viikko3/web-login/src/AppLibrary.py
@@ -0,0 +1,21 @@
+import requests
+
+
+class AppLibrary:
+    def __init__(self):
+        self._base_url = "http://localhost:5001"
+
+    def reset_application(self):
+        requests.post(f"{self._base_url}/tests/reset")
+
+    def create_user(self, username, password):
+        data = {
+            "username": username,
+            "password": password,
+            "password_confirmation": password
+        }
+
+        requests.post(f"{self._base_url}/register", data=data)
+
+    def go_to_starting_page(self):
+        requests.get(f"{self._base_url}/")
